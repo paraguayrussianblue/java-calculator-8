32b3d2a4e7c767b4825ccd6b3d57c37e2258bb44
gleaming9 <gleaming9@khu.ac.kr>
2025-10-20 00:12:39 +0900

    test: 잘못된 입력에 대한 에외 처리 테스트 추가
    - 구분자가 아닌 문자가 포함될 경우
    - 구분자가 연속되거나 마지막 위치일 경우
    - 잘못된 형식의 커스텀 구분자를 사용할 경우

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import calculator.service.CalculatorService;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    private final CalculatorService calculator = new CalculatorService();
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -27,5 +30,35 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    void 구분자가_아닌_문자_사용_1(){
+        assertThatThrownBy(() -> calculator.calculate("1,a,3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구분자가_아닌_문자_사용_2(){
+        assertThatThrownBy(() -> calculator.calculate("//;\\n1;4c4;4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 연속된_구분자() {
+        assertThatThrownBy(() -> calculator.calculate("1,,2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구분자로_끝나는_문자열() {
+        assertThatThrownBy(() -> calculator.calculate("//;\n1;43;"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 잘못된_커스텀_구분자_형식() {
+        assertThatThrownBy(() -> calculator.calculate("//;\n1:2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
